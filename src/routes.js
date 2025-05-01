107838220f15bcd84babc28410833a739e501da0
Weslei Possidonio <weslei.possidonio@gmail.com>
2025-05-01 20:50:10 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import ProfDataController from './app/controlles/ProfDataController.js'
 import UserController from './app/controlles/UserController.js'
 import authMiddlewares from './app/meddleawares/auth.js'
 import PaymentController from './app/controlles/PaymentController.js'
+import ConfirmEmailController from './app/controlles/ConfirmEmailController.js'
 
 const routes = new Router()
 
@@ -17,13 +18,22 @@ routes.post('/createAlunos', AlunosController.store)
 routes.get('/getHours', HorariosController.index)
 routes.post('/payment', upload, uploadToGoogleDrive, PaymentController.store)
 routes.get('/getProf', ProfDataController.index)
+routes.post('/confirmar-email', ConfirmEmailController.store)
+routes.patch('/updatePassword/:id', UserController.update)
+routes.patch('/updatePasswordProf/:id', ProfDataController.update)
+routes.patch('/updatePasswordStudents/:id', AlunosController.update)
 
 routes.use(authMiddlewares)
 routes.get('/getAlunos', AlunosController.index)
+routes.post('/atualizar-alunos', AlunosController.update)
 routes.post('/createHours', HorariosController.store)
 routes.post('/createProf', ProfDataController.store)
 routes.post('/createUser', UserController.store)
 routes.get('/getUser', UserController.index)
+
+routes.put('/updateUser/:id', UserController.update)
+routes.put('/updatedProf/:id', ProfDataController.update)
+routes.put('/updateStudents/:id', AlunosController.update)
 
 export default routes;
 
